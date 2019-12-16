9dccc3f6e84f1fae3f10bbda3d870e9d5634900f
skreebydba <skreebydba@users.noreply.github.com>
2019-12-16 10:07:54 -0600

    Saving after final runthrough

---
+++ BuildAzureVm.ps1
@@ -0,0 +1,16 @@
+﻿$rgname = "fbgsqlhsrg";
+$srvname = "fbgsqlhssrv";
+$dbname = "wwibc";
+$saname = "fbgsqlhssa";
+$login = "skreeby";
+$password = 'P@$$w0rd1';
+
+$importRequest = New-AzSqlDatabaseImport -ResourceGroupName $rgname `
+    -ServerName $srvname -DatabaseName $dbname `
+    -DatabaseMaxSizeBytes 1073741824 -StorageKeyType "StorageAccessKey" `
+    -StorageKey $(Get-AzStorageAccountKey `
+        -ResourceGroupName $rgname -StorageAccountName $saname).Value[0] `
+        -StorageUri "https://$saname.blob.core.windows.net/fbgsqlhsbak/WideWorldImporters-Standard.bacpac" `
+        -Edition "BusinessCritical" -ServiceObjectiveName "BC_Gen4_1" `
+        -AdministratorLogin $login `
+        -AdministratorLoginPassword $(ConvertTo-SecureString -String $password -AsPlainText -Force)
