bf7255e0c8068b7f3a1d8c506fe8582f78b225f2
skreebydba <skreebydba@users.noreply.github.com>
2019-12-17 13:24:57 -0600

    Removed PowerShell scripts

--- CreateContainerWithVariables.ps1
+++
@@ -1,5 +0,0 @@
-﻿$password = 'Th1s1s@P@$$w0rd';
-$containername = 'mycontainer';
-$port = 1437;
-$image = 'mcr.microsoft.com/mssql/server:2017-CU8-ubuntu';
-docker run -e 'ACCEPT_EULA=Y' -e "SA_PASSWORD=$password" --name $containername -p "$port:1433" -d $image
