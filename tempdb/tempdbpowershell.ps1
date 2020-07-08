e15dca5bc92db6b97ec5bc8a40fb6ec12afa5c53
Frank Gill <fgill@concurrency.com>
2020-07-08 18:15:11 +0000

    Initial commit of tempdb powershell

---
+++ tempdb/tempdbpowershell.ps1
@@ -0,0 +1,22 @@
+﻿$createdb = @"
+USE master;
+GO
+DROP DATABASE IF EXISTS ChicagoWhiteSox;
+GO
+CREATE DATABASE ChicagoWhiteSox;
+GO$createdb = @"
+USE master;
+GO
+DROP DATABASE IF EXISTS ChicagoWhiteSox;
+GO
+CREATE DATABASE ChicagoWhiteSox;
+GO
+USE ChicagoWhiteSox;
+GO
+CREATE OR ALTER PROCEDURE letsgosox
+AS
+CREATE TABLE #gosox (col1 INT);
+GO
+"@
+
+$instance = "localhost";
